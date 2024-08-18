efa611194fd21699e149a1d0ce1176c43d34eca0
Cam <2276730+camjm@users.noreply.github.com>
2024-08-18 14:40:53 +1200

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log(req.query);
+  console.log(req.body);
+  console.log(req.params);
+  res.render('Hey! It works!');
 });
+
 
 module.exports = router;
